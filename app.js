26e299c1665e31c2b8d9f119fa4ad72c4f8bcfa8
JC Gonzalez <programmer1718@gmail.com>
2022-06-25 22:45:55 -0400

    separate app setup from server entry point

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const generateErgoNameSvg = require("./utils/generateErgoNameSvg");
+
+const app = express();
+
+app.get("/generateSvg/raw/:ergoname", async (req, res) => {
+    const { ergoname } = req.params;
+    // TODO: Add input validation
+    const rawSvg = generateErgoNameSvg.raw(ergoname);
+    return res.send(rawSvg);
+});
+
+app.get("/generateSvg/encoded/:ergoname", async (req, res) => {
+    const { ergoname } = req.params;
+    // TODO: Add input validation
+    const encodedSvg = generateErgoNameSvg.encoded(ergoname);
+    return res.send(encodedSvg);
+});
+
+app.get("/generateSvg/url/:ergoname", async (req, res) => {
+    const { ergoname } = req.params;
+    // TODO: Add input validation
+    const svgUrl = generateErgoNameSvg.url(ergoname);
+    return res.send(svgUrl);
+});
+
+module.exports = app;
