322f53b48d1db764055e9ccda0ddef84adb302bc
JC Gonzalez <33580723+jaysee260@users.noreply.github.com>
2022-11-04 00:01:58 +0000

    update devcontainer.json to install some tools on postCreate

---
+++ scripts/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Clean up resources
+rm -rf ./aws ./awscliv2.zip
